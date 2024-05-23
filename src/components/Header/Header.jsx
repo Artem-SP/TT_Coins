4a15c985a8943742cdb29e767df2b4dd7f2e4e4c
Artem Sukhorukov <artem567gp@gmail.com>
2024-05-23 15:18:08 +0300

    Header_refactored

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,16 +7,15 @@ import styles from "./Header.module.scss";
 const Header = () => {
   const pathname = usePathname();
 
-  const linkTitle =
-    pathname === "/" ? "Multiple currencies prices" : " < Bitcoin/USD";
-  const linkPath = pathname === "/" ? "/multiple_coins" : "/";
-  const title =
-    pathname === "/" ? "Bitcoin / USD rate" : "Multiple currencies prices";
-
   return (
     <div className={styles.header}>
       <h4>{title}</h4>
-      <Link href={linkPath}> {linkTitle} </Link>
+
+      {pathname === "/" ? (
+        <Link href={linkPath}> Multiple currencies prices </Link>
+      ) : (
+        <Link href={linkPath}> &#767; Bitcoin/USD </Link>
+      )}
     </div>
   );
 };
